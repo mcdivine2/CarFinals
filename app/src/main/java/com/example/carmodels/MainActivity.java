350616b3321d2133e6519414e7f2bced2baa2bba
mcdivine2 <markdave.canoy@sccpag.edu.ph>
2023-12-09 21:46:37 +0800

    Register a user with A profile is done Splash Screen with internect connection

--- app/src/main/java/com/example/carmodels/MainActivity.java
+++ app/src/main/java/com/example/carmodels/MainActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity {
+    private static final int SPLASH_DELAY = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Check internet connectivity
+        if (NetworkUtils.isNetworkAvailable(this)) {
+            new Handler().postDelayed(() -> {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }, SPLASH_DELAY);
+        } else {
+            Toast.makeText(this, "Please check your internet connection", Toast.LENGTH_SHORT).show();
+        }
     }
 }
